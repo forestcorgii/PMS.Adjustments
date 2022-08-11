c01b0bccad491ea2381c00cc9851b2885b417dbe
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-08-11 15:21:25 +0800

    Initial implementation for Billings generation.

---
+++ Pms.Adjustments.Domain/Services/IGenerateBillingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Adjustments.Domain.Services
+{
+    public interface IGenerateBillingService
+    {
+        IEnumerable<Billing> GenerateBillingFromTimesheetView(string eeId,string cutoffId);
+        IEnumerable<Billing> GenerateBillingFromRecords(string eeId,string cutoffId);
+    }
+}
---
+++ Pms.Adjustments.ServiceLayer.EfCore/GenerateBillingService.cs
@@ -0,0 +1,23 @@
+﻿using Pms.Adjustments.Domain;
+using Pms.Adjustments.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Adjustments.ServiceLayer.EfCore
+{
+    public class GenerateBillingService : IGenerateBillingService
+    {
+        public IEnumerable<Billing> GenerateBillingFromRecords(string eeId, string cutoffId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Billing> GenerateBillingFromTimesheetView(string eeId, string cutoffId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

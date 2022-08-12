a232e7d7f153759dc264361f215cea7535d9036f
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-08-12 10:10:32 +0800

    Added for reminding if the billings has not been resetted.

---
+++ Pms.Adjustments.Domain/Exceptions/BillingAlreadyExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Adjustments.Domain.Exceptions
+{
+    public class BillingAlreadyExistsException : Exception
+    {
+        public string  BillingId { get; set; }
+        public override string Message => "Billing laready exists, have You already reset billings."; 
+
+        public BillingAlreadyExistsException(string billingId)
+        {
+            BillingId = billingId;
+        }
+
+    }
+}
